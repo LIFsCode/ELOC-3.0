ee5ac673b10a1eebdba65a9baf35f11f3c597494
OOHehir <40183643+OOHehir@users.noreply.github.com>
2023-10-29 20:59:27 +0000

    New methods added

--- eloc610LowPowerPartition/lib/wav_file/src/WAVFileWriter.cpp
+++ eloc610LowPowerPartition/lib/wav_file/src/WAVFileWriter.cpp
@@ -1,44 +1,75 @@
 #include "esp_log.h"
 #include "WAVFileWriter.h"
-#include "config.h"
 
-static const char *TAG = "WAV";
+static const char *TAG = "WAVFileWriter";
 
-WAVFileWriter::WAVFileWriter(FILE *fp, int sample_rate, int ch_count /*=1*/)
+WAVFileWriter::WAVFileWriter(FILE *fp, int sample_rate)
 {
   m_fp = fp;
-  setChannelCount(ch_count);
-  setSample_rate(sample_rate);
-  m_header.sample_rate = sample_rate;
+  m_header.setSample_rate(sample_rate);
   // write out the header - we'll fill in some of the blanks later
   fwrite(&m_header, sizeof(wav_header_t), 1, m_fp);
   m_file_size = sizeof(wav_header_t);
+
+  buf_select = 0;
+  buf_count = 0;
+  buffers[0][0] = {0};
+  buffers[1][0] = {0};
 }
 
-void WAVFileWriter::write(int16_t *samples, int count)
-{
-  // write the samples and keep track of the file size so far
-  fwrite(samples, sizeof(int16_t), count, m_fp);
-  m_file_size += sizeof(int16_t) * count;
+bool WAVFileWriter::buffer_is_full(){
+  
+  this->swap_buffers();
+  buf_ready = 1;
+
+  return true;
 }
 
-void WAVFileWriter::finish()
+void WAVFileWriter::swap_buffers(){
+  // TODO: Need thread mutex??
+  // swap buffers
+  if (++buf_select > 1)
+    buf_select = 0;
+  
+  buf_count = 0;
+
+  ESP_LOGI(TAG, "buffer_active = %d", buf_select);
+
+}
+
+void WAVFileWriter::write()
 {
+  auto buffer_inactive = buf_select ? 0 : 1;
+  
+  ESP_LOGI(TAG, "Writing wav file size: %d", m_file_size);
+  
+  if (m_fp == NULL){
+    ESP_LOGE(TAG, "File pointer is NULL");
+    return;
+  }
+
+  fwrite(buffers[buffer_inactive], sizeof(int16_t), buffer_size, m_fp);
+  m_file_size += sizeof(int16_t) * buffer_size;
+
+  // Don't swap buffers here, I2MEMSSampler::read() to do it
+  buf_ready = 0;
+}
+
+bool WAVFileWriter::finish()
+{
+  // Have to consider the case where file has reached its  
+  // max size & other buffer is being filled
   ESP_LOGI(TAG, "Finishing wav file size: %d", m_file_size);
   // now fill in the header with the correct information and write it again
   m_header.data_bytes = m_file_size - sizeof(wav_header_t);
   m_header.wav_size = m_file_size - 8;
   fseek(m_fp, 0, SEEK_SET);
   fwrite(&m_header, sizeof(wav_header_t), 1, m_fp);
-}
 
-void WAVFileWriter::setSample_rate (int sample_rate) {
-  m_header.sample_rate = sample_rate;
-  m_header.byte_rate = m_header.sample_rate * m_header.num_channels * (m_header.bit_depth / 8);
-}
+  m_file_size = 0;
+  m_fp = NULL;
+    
+  this->swap_buffers();
 
-void WAVFileWriter::setChannelCount(int channel_count) {
-    m_header.num_channels = channel_count;
-    m_header.byte_rate = m_header.sample_rate*2*channel_count;      // Number of bytes per second. sample_rate * num_channels * Bytes Per Sample
-    m_header.sample_alignment = channel_count*(m_header.bit_depth / 8); // num_channels * Bytes Per Sample
+  return true;
 }
--- eloc610LowPowerPartition/lib/wav_file/src/WAVFileWriter.h
+++ eloc610LowPowerPartition/lib/wav_file/src/WAVFileWriter.h
@@ -6,18 +6,84 @@
 class WAVFileWriter
 {
 private:
-  int m_file_size;
+  u_int32_t m_file_size; // Size of the file in bytes
 
-  FILE *m_fp;
+  FILE *m_fp = NULL;
   wav_header_t m_header;
-  /* TODO: check if it is better to have this functions as member of wav_header_t *
-    * however this leave wav_header_t as non POD struct.  */
-  void setSample_rate (int sample_rate);
-  void setChannelCount(int channel_count);
 
 public:
-  WAVFileWriter(FILE *fp, int sample_rate, int ch_count =1);
-  void start();
-  void write(int16_t *samples, int count);
-  void finish();
+  /**
+   * Use a double buffer system
+   * Active buffer -> fill with data
+   * Inactive buffer -> write to file
+   * These are public to alow access from I2SMEMSSampler
+   * @param buf_select which buffer is active?
+   * @param buf_count index of next element to write to
+   * @param buffers[2][16000] 2 buffers of 16000 elements each, 1 active, 1 inactive
+   */
+  size_t buf_select = 0;
+  int buf_ready = 0;
+  const static size_t buffer_size = 16000;
+  size_t buf_count;
+  int16_t buffers[2][buffer_size];
+
+  /**
+   * @brief Construct a new WAVFileWriter object
+   * @param fp file pointer
+   * @param sample_rate
+   */
+  WAVFileWriter(FILE *fp, int sample_rate);
+
+  /**
+   * @brief Get the current file size
+   * @return u_int32_t file size in bytes
+   */
+  u_int32_t get_file_size() { return m_file_size; }
+
+  /**
+   * @brief Set current file size to 0
+   */
+  void zero_file_size() { m_file_size = 0; }
+
+  /**
+   * @brief Get wether file ready to save
+   * @return true if ready to save
+   */
+  int ready_to_save() {return buf_ready;}
+
+  /**
+   * @brief Called when a buffer is full
+   * @note This will swap the buffers and set buf_ready
+   * 
+   * @return true success
+   * @return false 
+   */
+  bool buffer_is_full();
+
+  /**
+   * @brief Swap buffers
+   *
+   */
+  void swap_buffers();
+
+  /**
+   * @brief Write samples to file
+   */
+  void write();
+
+  /**
+   * @brief Create header and write to file
+   * @note This will reset the buf_count to 0
+   * @return true success
+   */
+  bool finish();
+
+  // /**
+  //  * @brief Transfer contents from external buffer to internal buffer
+  //  * @param external_buffer
+  //  * @param external_buffer_size number of elements in external_buffer
+  //  * @return true success
+  //  * @return false
+  //  */
+  // bool pack_buffer(&external_buffer, size_t external_buffer_size);
 };

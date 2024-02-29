3d4518c108b9e8304bf054d6be88be7f647149c2
OOHehir <40183643+OOHehir@users.noreply.github.com>
2024-02-29 14:02:22 +0000

    Remove intermediate bool

--- eloc610LowPowerPartition/src/main.cpp
+++ eloc610LowPowerPartition/src/main.cpp
@@ -894,7 +894,6 @@ void app_main(void) {
 
     // This might be redundant, set directly in ElocCommands.cpp
     auto new_mode =  WAVFileWriter::Mode::disabled;
-    auto change_ai_run_enable = false;
 
     while (true) {
         if (xQueueReceive(rec_req_evt_queue, &new_mode, pdMS_TO_TICKS(500))) {
@@ -958,18 +957,16 @@ void app_main(void) {
 
         #ifdef EDGE_IMPULSE_ENABLED
 
-        if (xQueueReceive(rec_ai_evt_queue, &change_ai_run_enable, pdMS_TO_TICKS(500))) {
-            ESP_LOGI(TAG, "Received AI run enable = %d", change_ai_run_enable);
+        if (xQueueReceive(rec_ai_evt_queue, &ai_run_enable, pdMS_TO_TICKS(500))) {
+            ESP_LOGI(TAG, "Received AI run enable = %d", ai_run_enable);
             auto ei_status = (edgeImpulse.get_status() == EdgeImpulse::Status::running ? "running" : "not running");
             ESP_LOGI(TAG, "EI current status = %s (%d)", ei_status, static_cast<int>(edgeImpulse.get_status()));
 
-            if (change_ai_run_enable == false && (edgeImpulse.get_status() == EdgeImpulse::Status::running)) {
+            if (ai_run_enable == false && (edgeImpulse.get_status() == EdgeImpulse::Status::running)) {
                 ESP_LOGI(TAG, "Stopping EI thread");
-                ai_run_enable = false;
                 edgeImpulse.set_status(EdgeImpulse::Status::not_running);
-            } else if (change_ai_run_enable == true && (edgeImpulse.get_status() == EdgeImpulse::Status::not_running)) {
+            } else if (ai_run_enable == true && (edgeImpulse.get_status() == EdgeImpulse::Status::not_running)) {
                 ESP_LOGI(TAG, "Starting EI thread");
-                ai_run_enable = true;
                 if (edgeImpulse.start_ei_thread(ei_callback_func) != ESP_OK) {
                     ESP_LOGE(TAG, "Failed to start EI thread");
                     // Should this be retried?

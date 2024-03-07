27db3f78a94fb96734ee5d27dab7eb5f908b838a
OOHehir <40183643+OOHehir@users.noreply.github.com>
2024-03-07 10:52:41 +0000

    Increase precision of times

--- eloc610LowPowerPartition/src/Commands/ElocCommands.cpp
+++ eloc610LowPowerPartition/src/Commands/ElocCommands.cpp
@@ -119,7 +119,7 @@ void printStatus(String& buf) {
     RecState recState = calcRecordingState();
     addEnum(recordingState, recState);
 
-    session["recordingTime[h]"]    = round((wav_writer.get_recordingTimeSinceLastStarted_usec() / 1000.f / 1000.f / 60.f / 60.f), 2);
+    session["recordingTime[h]"]    = round((wav_writer.get_recordingTimeSinceLastStarted_usec() / 1000.f / 1000.f / 60.f / 60.f), 3);
     JsonObject ai = session.createNestedObject("detection");
     ai["state"]                   = ai_run_enable;
     // first set to defaults in case edge impulse is not included in binary
@@ -127,14 +127,14 @@ void printStatus(String& buf) {
     ai["detectedEvents"]          = 0;
     ai["aiModel"]                 = "";
 #ifdef EDGE_IMPULSE_ENABLED
-    ai["detectingTime[h]"]        = round((edgeImpulse.get_totalDetectingTime_secs() / 60.f / 60.f), 2);
+    ai["detectingTime[h]"]        = round((edgeImpulse.get_totalDetectingTime_secs() / 60.f / 60.f), 3);
     ai["detectedEvents"]          = edgeImpulse.get_detectedEvents();
     ai["aiModel"]                 = EI_CLASSIFIER_PROJECT_NAME;
 #endif
     JsonObject device = doc.createNestedObject("device");
     device["firmware"]                   = gFirmwareVersion;
-    device["Uptime[h]"]                  = round((esp_timer_get_time() / 1000.f / 1000.f / 60.f / 60.f), 2);
-    device["totalRecordingTime[h]"]      = round((wav_writer.get_recording_time_total_sec() / 60.f / 60.f), 2);
+    device["Uptime[h]"]                  = round((esp_timer_get_time() / 1000.f / 1000.f / 60.f / 60.f), 3);
+    device["totalRecordingTime[h]"]      = round((wav_writer.get_recording_time_total_sec() / 60.f / 60.f), 3);
 
     float sdCardSizeGB = 0;
     float sdCardFreeSpaceGB = 0;

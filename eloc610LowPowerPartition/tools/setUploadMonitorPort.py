d7def94eb77727ef92369977d7c2b2418fa6df0c
OOHehir <40183643+OOHehir@users.noreply.github.com>
2024-04-28 15:34:11 +0100

    Add script to set appropriate upload & monitor port depending on OS #91

---
+++ eloc610LowPowerPartition/tools/setUploadMonitorPort.py
@@ -0,0 +1,43 @@
+#
+# Sets OS specific upload_port & monitor_port if not already set
+# This script is called by platformio.ini
+#
+
+Import("env")     # Yes, starts with a capital letter. This is not a typo.
+import sys
+
+WINDOWS_PORT = "COM3"
+LINUX_PORT = "/dev/ttyUSB0"
+
+# CAUTION
+# Even if MONITOR_PORT is set in platformio.ini doesn't
+# seem to be available when this script is run. So, we
+# test for UPLOAD_PORT & set MONITOR_PORT to the same
+SET_MONITOR_PORT = True
+
+IS_WINDOWS = sys.platform.startswith("win")
+IS_LINUX = sys.platform.startswith("linux")
+IS_MAC = sys.platform.startswith("darwin")
+
+print("Running setUploadMonitorPort.py...")
+# print("UPLOAD_PORT = ", env.get("UPLOAD_PORT"))
+# print("MONITOR_PORT = ", env.get("MONITOR_PORT"))
+# print(env.Dump())
+
+if (env.get("UPLOAD_PORT") == None):
+    if IS_WINDOWS:
+        env.Replace(UPLOAD_PORT=WINDOWS_PORT)
+    elif IS_LINUX:
+        env.Replace(UPLOAD_PORT=LINUX_PORT)
+    elif IS_MAC:
+        new_build_dir = "$PROJECT_DIR/.pio/build_custom_mac/$PIOENV"
+    else:
+        sys.stderr.write("Unrecognized OS.\n")
+        env.Exit(-1)
+    print("upload_port set to", env.get("UPLOAD_PORT"))
+else:
+    print("upload_port already set to", env.get("UPLOAD_PORT"))
+    
+if (SET_MONITOR_PORT):
+    env.Replace(MONITOR_PORT=(env.get("UPLOAD_PORT")))
+    print("monitor_port set to", env.get("MONITOR_PORT"))

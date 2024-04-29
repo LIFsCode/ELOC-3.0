34be4088da709161ad6c836d5f32e80382762a44
OOHehir <40183643+OOHehir@users.noreply.github.com>
2024-04-29 06:00:32 +0100

    Add method

--- eloc610LowPowerPartition/lib/utils/src/ffsutils.cpp
+++ eloc610LowPowerPartition/lib/utils/src/ffsutils.cpp
@@ -199,21 +199,25 @@ sdTestSpeed_t TestSDFile(const char *path, uint8_t *buf, int len, int TEST_FILE_
 }
 
 int getFileListWithExtension(const char *path, const char *extension, std::vector<std::string> &files) {
+    static const char* TAG = "getFileListWithExtension";
+
     DIR *dir;
     struct dirent *ent;
-    // if ((dir = opendir(path)) != NULL) {
-    //     while ((ent = readdir(dir)) != NULL) {
-    //         if (ent->d_type == DT_REG) {
-    //             String filename = ent->d_name;
-    //             if (filename.find(extension) != std::string::npos) {
-    //                 files.push_back(filename);
-    //             }
-    //         }
-    //     }
-    //     closedir(dir);
-    // } else {
-    //     return -1;
-    // }
+
+    if ((dir = opendir(path)) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_type == DT_REG) {
+                std::string fname = ent->d_name;
+                if (fname.find(extension) != std::string::npos) {
+                    ESP_LOGI(TAG, "Found filename: %s", fname.c_str());
+                    files.push_back(fname);
+                }
+            }
+        }
+        closedir(dir);
+    } else {
+        return -1;
+    }
     return 0;
 }
 

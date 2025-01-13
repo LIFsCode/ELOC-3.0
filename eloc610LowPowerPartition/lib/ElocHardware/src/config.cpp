8f8f17a88f51dcf180989a64397255325f9d6bd7
OOHehir <40183643+OOHehir@users.noreply.github.com>
2025-01-13 00:24:43 +0000

    Update PlatformIO espressif32 SDK

--- eloc610LowPowerPartition/lib/ElocHardware/src/config.cpp
+++ eloc610LowPowerPartition/lib/ElocHardware/src/config.cpp
@@ -12,15 +12,15 @@ i2s_config_t i2s_mic_Config = {
         .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
     #endif
 
-    .communication_format = I2S_COMM_FORMAT_I2S,
+    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
     .intr_alloc_flags = I2S_INTR_PIRO,
     .dma_buf_count = I2S_DMA_BUFFER_COUNT,  //  so 2000 sample  buffer at 16khz sr gives us 125ms to do our writing
     .dma_buf_len = I2S_DMA_BUFFER_LEN,      //  8 buffers gives us half  second
     .use_apll = true,                       //  the only thing that works with LowPower/APLL is 16khz 12khz??
     .tx_desc_auto_clear = false,
     .fixed_mclk = 0,
-    //  .mclk_multiple =I2S_MCLK_MULTIPLE_DEFAULT,   // I2S_MCLK_MULTIPLE_DEFAULT= 0,       /*!< Default value. mclk = sample_rate * 256 */
-    //  .bits_per_chan=I2S_BITS_PER_CHAN_DEFAULT
+    .mclk_multiple =I2S_MCLK_MULTIPLE_DEFAULT,   // I2S_MCLK_MULTIPLE_DEFAULT= 0,       /*!< Default value. mclk = sample_rate * 256 */
+    .bits_per_chan=I2S_BITS_PER_CHAN_DEFAULT
     };
 
 // i2s microphone pins
@@ -53,13 +53,13 @@ lis3dh_int_click_config_t lis3dh_click_config = {
     .threshold = 30,     /* threshold which is used by the system to start the click-detection procedure */
     .latch = false,      /* true:  the interrupt is kept high until CLICK_SRC (39h) is read
                           * false: the interrupt is kept high for the duration of the latency window.  */
-    .time_limit   = 10,  /* define the maximum time interval that can elapse between the start of 
-                          * the click-detection procedure (the acceleration on the selected channel exceeds the 
+    .time_limit   = 10,  /* define the maximum time interval that can elapse between the start of
+                          * the click-detection procedure (the acceleration on the selected channel exceeds the
                           * programmed threshold) and when the acceleration falls back below the threshold. */
-    .time_latency = 20,  /* define the time interval that starts after the first click detection where 
-                          * the click-detection procedure is disabled, in cases where the device is configured for 
+    .time_latency = 20,  /* define the time interval that starts after the first click detection where
+                          * the click-detection procedure is disabled, in cases where the device is configured for
                           * double-click detection */
-    .time_window  = 255, /* define the maximum interval of time that can elapse after the end of the 
-                          * latency interval in which the click-detection procedure can start, in cases where the device 
+    .time_window  = 255, /* define the maximum interval of time that can elapse after the end of the
+                          * latency interval in which the click-detection procedure can start, in cases where the device
                           * is configured for double-click detection. */
 };

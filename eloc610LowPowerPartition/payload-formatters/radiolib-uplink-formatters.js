8455b44859c1960509dd29af7a017f547053dcfe
LIFsCode <109753539+LIFsCode@users.noreply.github.com>
2025-02-08 19:41:15 +0100

    Extend payload formaters to basic idea about Lora Information

--- eloc610LowPowerPartition/payload-formatters/radiolib-uplink-formatters.js
+++ eloc610LowPowerPartition/payload-formatters/radiolib-uplink-formatters.js
@@ -11,13 +11,57 @@
  ******************************************************************************/
 
 function decodeUplink(input) {
-    var val1, val2;
-    val1 = input.bytes[0];
-    val2 = (input.bytes[1] << 8 | input.bytes[2]);
+    var msgType;
+    msgType = input.bytes[0];
+    var batterySoC, timestamp, recordingState;
+    var state
+    switch(msgType) {
+        case 0:     // status messages
+            timestamp = (input.bytes[1] << 24 | input.bytes[2] << 16 | input.bytes[3] << 8 | input.bytes[4]);
+            var date = new Date(timestamp * 1000);
+            batterySoC = input.bytes[5];
+            switch (input.bytes[6]) {
+                case 0:
+                    recordingState = "recordOff_detectOff";
+                    break;
+                case 1:
+                    recordingState = "recordOn_detectOff";
+                    break;
+                case 2:
+                    recordingState = "recordOn_detectOn";
+                    break;
+                case 3:
+                    recordingState = "recordOff_detectOn";
+                    break;
+                case 4:
+                    recordingState = "recordOnEvent";
+                    break;
+                default:
+                    recordingState = "recInvalid";
+                    break;
+            }
+            return {
+                data: {
+                msgType: msgType,
+                time: date,
+                state: recordingState,
+                Battery: batterySoC,
+                bytes: input.bytes
+                },
+                warnings: [],
+                errors: []
+            };
+          break;
+        case 1: // event notfier
+          // code block
+          break;
+        default:
+          // code block
+      } 
+
     return {
         data: {
-        counter1: val1,
-        counter2: val2,
+        msgType: msgType,
         bytes: input.bytes
         },
         warnings: [],
